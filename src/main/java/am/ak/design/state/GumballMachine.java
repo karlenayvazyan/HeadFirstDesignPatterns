417f00956ae1a261a3f66f6aac41bbcf3acb336c
karlen <karlen.ayvazyan@gmail.com>
2020-07-07 21:29:08 +0600

    State pattern

---
+++ src/main/java/am/ak/design/state/GumballMachine.java
@@ -0,0 +1,75 @@
+package am.ak.design.state;
+
+public class GumballMachine {
+
+    private final State soldOutState;
+    private final State noQuarterState;
+    private final State hasQuarterState;
+    private final State soldState;
+    private final State winnerState;
+
+    private State state;
+    private int count = 0;
+
+    public GumballMachine(int count) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        winnerState = new WinnerState(this);
+        this.count = count;
+        this.state = soldOutState;
+        if (count > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getWinnerState() {
+        return winnerState;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+        state.dispense();
+    }
+
+    public void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count = count - 1;
+        }
+    }
+}
+
---
+++ src/test/java/am/ak/design/state/GumballMachineTest.java
@@ -0,0 +1,20 @@
+package am.ak.design.state;
+
+import org.junit.jupiter.api.Test;
+
+class GumballMachineTest {
+
+    @Test
+    public void test() {
+        GumballMachine gumballMachine = new GumballMachine(5);
+        System.out.println(gumballMachine);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+    }
+}
